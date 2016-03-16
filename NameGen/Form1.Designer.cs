9d6a9c0aaf2fc60cf2ebc48a773ab7f15180e2e4
kubawich <kubawich45@gmail.com>
2016-03-16 16:39:26 +0100

    Initial commit

---
+++ NameGen/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace NameGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWynik = new System.Windows.Forms.Label();
+            this.btnGeneruj = new System.Windows.Forms.Button();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.btnAddName = new System.Windows.Forms.Button();
+            this.btnAddSurname = new System.Windows.Forms.Button();
+            this.SurnameBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblWynik
+            // 
+            this.lblWynik.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWynik.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblWynik.Location = new System.Drawing.Point(69, 9);
+            this.lblWynik.Name = "lblWynik";
+            this.lblWynik.Size = new System.Drawing.Size(154, 23);
+            this.lblWynik.TabIndex = 0;
+            this.lblWynik.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGeneruj
+            // 
+            this.btnGeneruj.Location = new System.Drawing.Point(69, 35);
+            this.btnGeneruj.Name = "btnGeneruj";
+            this.btnGeneruj.Size = new System.Drawing.Size(154, 23);
+            this.btnGeneruj.TabIndex = 1;
+            this.btnGeneruj.Text = "Generuj";
+            this.btnGeneruj.UseVisualStyleBackColor = true;
+            this.btnGeneruj.Click += new System.EventHandler(this.StartGenerate);
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(12, 103);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(108, 20);
+            this.NameBox.TabIndex = 2;
+            // 
+            // btnAddName
+            // 
+            this.btnAddName.Location = new System.Drawing.Point(12, 129);
+            this.btnAddName.Name = "btnAddName";
+            this.btnAddName.Size = new System.Drawing.Size(108, 23);
+            this.btnAddName.TabIndex = 3;
+            this.btnAddName.Text = "Dodaj Imię";
+            this.btnAddName.UseVisualStyleBackColor = true;
+            this.btnAddName.Click += new System.EventHandler(this.AddName);
+            // 
+            // btnAddSurname
+            // 
+            this.btnAddSurname.Location = new System.Drawing.Point(163, 129);
+            this.btnAddSurname.Name = "btnAddSurname";
+            this.btnAddSurname.Size = new System.Drawing.Size(109, 23);
+            this.btnAddSurname.TabIndex = 4;
+            this.btnAddSurname.Text = "Dodaj Nazwisko";
+            this.btnAddSurname.UseVisualStyleBackColor = true;
+            this.btnAddSurname.Click += new System.EventHandler(this.AddSurname);
+            // 
+            // SurnameBox
+            // 
+            this.SurnameBox.Location = new System.Drawing.Point(163, 103);
+            this.SurnameBox.Name = "SurnameBox";
+            this.SurnameBox.Size = new System.Drawing.Size(108, 20);
+            this.SurnameBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.SurnameBox);
+            this.Controls.Add(this.btnAddSurname);
+            this.Controls.Add(this.btnAddName);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.btnGeneruj);
+            this.Controls.Add(this.lblWynik);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWynik;
+        private System.Windows.Forms.Button btnGeneruj;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button btnAddName;
+        private System.Windows.Forms.Button btnAddSurname;
+        private System.Windows.Forms.TextBox SurnameBox;
+    }
+}
+
---
+++ NameGen/Form1.cs
@@ -0,0 +1,83 @@
+﻿/*Created by Jakub Wichliński github.com/kubawich
+
+Name generator is 1st out of 100 apps made with contest training app list (tnij.org/trainingapps)
+
+TODO: 
+Fix multiplication of predefinied list values
+Let program draw all name/surname values into listView
+Add working progress bar
+Refactoring & comments
+Improve design and UX(maybe rewrite to wpf using material design template)*/
+
+using System;
+using System.Collections.Generic;
+using System.Media;
+using System.Windows.Forms;
+
+namespace NameGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGenerate(object sender, EventArgs e)
+        {
+            SystemSound genSound = SystemSounds.Beep;
+            lblWynik.Text = Generate.NameGen() + " " + Generate.SurnameGen();
+            genSound.Play();
+            Console.WriteLine("Generating: " + lblWynik.Text);
+        }
+
+        private void AddName(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(NameBox.Text.ToString()))
+                MessageBox.Show("Error: field cannot be empty");
+            else {
+                Generate.LName.Add(NameBox.Text.ToString());
+                Console.WriteLine("Adding new name to index: " + NameBox.Text);
+                Generate.LName.ForEach(item => Console.WriteLine(item));
+                NameBox.Clear();
+            }
+        }
+
+        private void AddSurname(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(SurnameBox.Text.ToString()))
+                MessageBox.Show("Error: field cannot be empty");
+            else {
+                Generate.LName.Add(SurnameBox.Text.ToString());
+                Console.WriteLine("Adding new name to index: " + SurnameBox.Text);
+                Generate.LSurname.ForEach(item => Console.WriteLine(item));
+                SurnameBox.Clear();
+            }
+        }
+    }
+
+    public class Generate
+    {
+        static string[] Name = new string[] { "Mariusz", "Janusz", "Bogdan", "Zbigniew" };
+        static string[] Surname = new string[] { "Stonoga", "Wałęsa", "Białkov", "Wichliński" };
+
+        public static List<string> LName = new List<string>();
+        public static List<string> LSurname = new List<string>();
+
+        static Random random = new Random();
+
+        public static string NameGen()
+        {
+            LName.AddRange(Name);
+            int NameIndex = random.Next(LName.Count);
+            return LName[NameIndex];
+        }
+
+        public static string SurnameGen()
+        {
+            LSurname.AddRange(Surname);
+            int NameIndex = random.Next(LSurname.Count);
+            return LSurname[NameIndex];
+        }
+    }
+}
